e66bb93d7a2ffad6b2a07866a16020b06bc2e24a
mats.aa <amats6655@gmail.com>
2022-10-04 15:24:14 +0300

    Изменен способ доступа к базе данных, почищен MainWindow.xaml

--- equipment/MainWindow.xaml.cs
+++ equipment/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using equipment.equipment_rentDataSetTableAdapters;
 using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -11,123 +14,147 @@ namespace equipment
     /// </summary>
     public partial class MainWindow : Window
     {
+        string connectionString;
+        SqlDataAdapter adapter;
+        DataTable usersTable;
+        DataTable equipmentsTable;
+        DataTable ordersTable;
+
 
         public MainWindow()
         {
             InitializeComponent();
+            connectionString = ConfigurationManager.ConnectionStrings["equipmentDbConnect"].ConnectionString;
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            equipment_rentDataSet equipment_rentDataSet = (equipment_rentDataSet)FindResource("equipment_rentDataSet");
+            
+        }
+        /// <summary>
+        /// Загрузка данных в TabItem
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CMO_users(object sender, RoutedEventArgs e)
+        {
+            string sql = "SELECT * FROM users";
+            usersTable = new DataTable();
+            SqlConnection connection = null;
+            try
+            {
+                connection = new SqlConnection(connectionString);
+                SqlCommand command = new SqlCommand(sql, connection);
+                adapter = new SqlDataAdapter(command);
+                
+                connection.Open();
+                adapter.Fill(usersTable);
+                UsersGrid.ItemsSource = usersTable.DefaultView;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (connection != null)
+                    connection.Close();
+            }
+        }
 
-            // Загрузить данные в таблицу users
-            usersTableAdapter equipment_rentDataSetusersTableAdapter = new usersTableAdapter();
-            equipment_rentDataSetusersTableAdapter.Fill(equipment_rentDataSet.users);
-            CollectionViewSource usersViewSource = (CollectionViewSource)FindResource("usersViewSource");
-            usersViewSource.View.MoveCurrentToFirst();
+        private void CMO_equipments(object sender, RoutedEventArgs e)
+        {
+            string sql = "SELECT * FROM dbo.equipment";
+            equipmentsTable = new DataTable();
+            SqlConnection connection = null;
+            try
+            {
+                connection = new SqlConnection(connectionString);
+                SqlCommand command = new SqlCommand(sql, connection);
+                adapter = new SqlDataAdapter(command);
 
-            // Загрузить данные в таблицу types
-            equip_typesTableAdapter equipment_rentDataSetequip_typesTableAdapter = new equip_typesTableAdapter();
-            equipment_rentDataSetequip_typesTableAdapter.Fill(equipment_rentDataSet.equip_types);
+                connection.Open();
+                adapter.Fill(equipmentsTable);
+                EquipmentsGrid.ItemsSource = equipmentsTable.DefaultView;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (connection != null)
+                    connection.Close();
+            }
+        }
 
-            // Загрузить данные в таблицу equipment
-            equipmentTableAdapter equipment_rentDataSetequipmentTableAdapter = new equipmentTableAdapter();
-            equipment_rentDataSetequipmentTableAdapter.Fill(equipment_rentDataSet.equipment);
-            CollectionViewSource equipmentViewSource = (CollectionViewSource)FindResource("equipmentViewSource");
-            equipmentViewSource.View.MoveCurrentToFirst();
+        private void CMO_orders(object sender, RoutedEventArgs e)
+        {
+            string sql = "SELECT * FROM dbo.order";
+            ordersTable = new DataTable();
+            SqlConnection connection = null;
+            try
+            {
+                connection = new SqlConnection(connectionString);
+                SqlCommand command = new SqlCommand(sql, connection);
+                adapter = new SqlDataAdapter(command);
 
-            // Загрузить данные в таблицу order
-            orderTableAdapter equipment_rentDataSetorderTableAdapter = new orderTableAdapter();
-            equipment_rentDataSetorderTableAdapter.Fill(equipment_rentDataSet.order);
-            CollectionViewSource orderViewSource = (CollectionViewSource)FindResource("orderViewSource");
-            orderViewSource.View.MoveCurrentToFirst();
-
-            // Загрузить данные в таблицу equip
-            equipTableAdapter equipment_rentDataSetequipTableAdapter = new equipTableAdapter();
-            equipment_rentDataSetequipTableAdapter.Fill(equipment_rentDataSet.equip);
-            CollectionViewSource equipViewSource = (CollectionViewSource)FindResource("equipViewSource");
-            equipViewSource.View.MoveCurrentToFirst();
+                connection.Open();
+                adapter.Fill(ordersTable);
+                OrdersGrid.ItemsSource = ordersTable.DefaultView;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (connection != null)
+                    connection.Close();
+            }
         }
 
         private void btn_users_add_Click(object sender, RoutedEventArgs e)
         {
-            usersTableAdapter equipment_rentDataSetusersTableAdapter = new usersTableAdapter();
-            equipment_rentDataSetusersTableAdapter.Insert(tb_users_name.Text, tb_users_phone.Text, cb_users_admin.IsChecked);
+            //usersTableAdapter equipment_rentDataSetusersTableAdapter = new usersTableAdapter();
+            //equipment_rentDataSetusersTableAdapter.Insert(tb_users_name.Text, tb_users_phone.Text, cb_users_admin.IsChecked);
 
-            equipment_rentDataSet equipment_rentDataSet = (equipment_rentDataSet)FindResource("equipment_rentDataSet");
-            equipment_rentDataSetusersTableAdapter.Fill(equipment_rentDataSet.users);
-            tb_users_name.Clear();
-            tb_users_phone.Clear();
+            //equipment_rentDataSet equipment_rentDataSet = (equipment_rentDataSet)FindResource("equipment_rentDataSet");
+            //equipment_rentDataSetusersTableAdapter.Fill(equipment_rentDataSet.users);
+            //tb_users_name.Clear();
+            //tb_users_phone.Clear();
         }
 
         private void btn_equip_add_Click(object sender, RoutedEventArgs e)
         {
-            equipTableAdapter equipment_rentDataSetEquipTableAdapter = new equipTableAdapter();
-            equipment_rentDataSetEquipTableAdapter.Insert((int)cb_equip_type.SelectedValue, tb_equip_model.Text, int.Parse(tb_equip_amount.Text), int.Parse(tb_equip_amount.Text));
+            //equipTableAdapter equipment_rentDataSetEquipTableAdapter = new equipTableAdapter();
+            //equipment_rentDataSetEquipTableAdapter.Insert((int)cb_equip_type.SelectedValue, tb_equip_model.Text, int.Parse(tb_equip_amount.Text), int.Parse(tb_equip_amount.Text));
 
-            equipment_rentDataSet equipment_rentDataSet = (equipment_rentDataSet)FindResource("equipment_rentDataSet");
-            equipmentTableAdapter equipment_rentDataSetequipmentTableAdapter = new equipmentTableAdapter();
-            equipment_rentDataSetequipmentTableAdapter.Fill(equipment_rentDataSet.equipment);
-            tb_equip_model.Clear();
-            tb_equip_amount.Clear();
+            //equipment_rentDataSet equipment_rentDataSet = (equipment_rentDataSet)FindResource("equipment_rentDataSet");
+            //equipmentTableAdapter equipment_rentDataSetequipmentTableAdapter = new equipmentTableAdapter();
+            //equipment_rentDataSetequipmentTableAdapter.Fill(equipment_rentDataSet.equipment);
+            //tb_equip_model.Clear();
+            //tb_equip_amount.Clear();
         }
 
         private void Btn_orders_add_Click(object sender, RoutedEventArgs e)
         {
-            ordersTableAdapter equipment_rentDataSetordersTableAdapter = new ordersTableAdapter();
-            equipment_rentDataSetordersTableAdapter.Insert((int)cb_orders_user.SelectedValue, (int)cb_orders_eqiup.SelectedValue, int.Parse(tb_orders_amount.Text),
-                DateTime.Parse(dp_orders_issue.Text), DateTime.Parse(dp_orders_return.Text), false);
+        //    ordersTableAdapter equipment_rentDataSetordersTableAdapter = new ordersTableAdapter();
+        //    equipment_rentDataSetordersTableAdapter.Insert((int)cb_orders_user.SelectedValue, (int)cb_orders_eqiup.SelectedValue, int.Parse(tb_orders_amount.Text),
+        //        DateTime.Parse(dp_orders_issue.Text), DateTime.Parse(dp_orders_return.Text), false);
 
-            equipment_rentDataSet equipment_rentDataSet = (equipment_rentDataSet)FindResource("equipment_rentDataSet");
-            orderTableAdapter equipment_rentDataSetorderTableAdapter = new orderTableAdapter();
-            equipment_rentDataSetorderTableAdapter.Fill(equipment_rentDataSet.order);
-            tb_orders_amount.Clear();
+        //    equipment_rentDataSet equipment_rentDataSet = (equipment_rentDataSet)FindResource("equipment_rentDataSet");
+        //    orderTableAdapter equipment_rentDataSetorderTableAdapter = new orderTableAdapter();
+        //    equipment_rentDataSetorderTableAdapter.Fill(equipment_rentDataSet.order);
+        //    tb_orders_amount.Clear();
         }
 
 
 
-        private void Bold_Checked(object sender, RoutedEventArgs e)
-        {
-            DG_orders.FontWeight = FontWeights.Bold;
-        }
-
-        private void Bold_Unchecked(object sender, RoutedEventArgs e)
-        {
-            DG_orders.FontWeight = FontWeights.Normal;
-        }
-
-        private void Italic_Checked(object sender, RoutedEventArgs e)
-        {
-            DG_orders.FontStyle = FontStyles.Italic;
-        }
-
-        private void Italic_Unchecked(object sender, RoutedEventArgs e)
-        {
-            DG_orders.FontStyle = FontStyles.Normal;
-        }
-
-        private void IncreaseFont_Click(object sender, RoutedEventArgs e)
-        {
-            if (DG_orders.FontSize < 18)
-            {
-                DG_orders.FontSize += 2;
-            }
-        }
-
-        private void DecreaseFont_Click(object sender, RoutedEventArgs e)
-        {
-            if (DG_orders.FontSize > 10)
-            {
-                DG_orders.FontSize -= 2;
-            }
-        }
 
         private void ReturnOrder_Click(object sender, RoutedEventArgs e)
         {
             
         }
-
-
     }
 }
